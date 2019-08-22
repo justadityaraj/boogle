4ee3bc89668f995a93599e358a782ea4cef63318
Aditya Raj <justadityaraj@yahoo.com>
2019-08-22 20:54:24 +0530

    css

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Boogle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
     <div class="mainSection">
         <div class="logoContainer">
@@ -13,10 +16,11 @@
         </div>
         <div class="searchContainer">
             <form action="search.php" method="GET">
-            <input class="searchBox" type="text" name="term" id="" placeholder="Boogle anything...">
-            <input class="searchButton" type="submit" value="Search" id="">
+                <input class="searchBox" type="text" name="term" id="" placeholder="Boogle anything...">
+                <input class="searchButton" type="submit" value="Search" id="">
             </form>
         </div>
     </div>
 </body>
+
 </html>
