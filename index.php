f0ba81126d1bde494365c28deb779bae3f5155bd
Aditya Raj <justadityaraj@yahoo.com>
2019-08-22 20:43:58 +0530

    formActionGET

--- index.php
+++ index.php
@@ -12,8 +12,10 @@
             <img src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Boogle">
         </div>
         <div class="searchContainer">
+            <form action="search.php" method="GET">
             <input class="searchBox" type="text" name="term" id="" placeholder="Boogle anything...">
             <input class="searchButton" type="submit" value="Search" id="">
+            </form>
         </div>
     </div>
 </body>
