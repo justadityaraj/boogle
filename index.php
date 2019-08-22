f9445c8fdc807069df5d43eb972aaa31e6006ddb
Aditya Raj <justadityaraj@yahoo.com>
2019-08-22 20:41:00 +0530

    wipStructure

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
-<?php //vscode git settings check - commit config.
-//atom
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boogle</title>
+</head>
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Boogle">
+        </div>
+        <div class="searchContainer">
+            <input class="searchBox" type="text" name="term" id="" placeholder="Boogle anything...">
+            <input class="searchButton" type="submit" value="Search" id="">
+        </div>
+    </div>
+</body>
+</html>
