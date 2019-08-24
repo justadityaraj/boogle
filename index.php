0608cb81f6e8b68b270d2a1b92545cd47a6ea13a
Aditya Raj <justadityaraj@yahoo.com>
2019-08-24 21:08:11 +0530

    Boogle logo

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <div class="wrapper indexPage">
         <div class="mainSection">
             <div class="logoContainer">
-                <img src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Boogle">
+                <img src="assets/images/boogle-logo.png" alt="Boogle">
             </div>
             <div class="searchContainer">
                 <form action="search.php" method="GET">
