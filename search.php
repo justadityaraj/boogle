d175b65d0ce4fc330ec652bf3fb321f61e658960
Aditya Raj <justadityaraj@yahoo.com>
2019-08-24 21:20:05 +0530

    search: headerContent

--- search.php
+++ search.php
@@ -1,2 +1,24 @@
-<?php
-echo $_GET["term"];
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boogle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                <div class="logoContainer">
+                    <img src="assets/images/boogle-logo.png" alt="">
+                </div> 
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
