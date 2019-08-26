2c64e9b5966537cd40b42797ebd5b5f8879c4f78
Aditya Raj <justadityaraj@yahoo.com>
2019-08-26 18:19:50 +0530

    .searchButton

--- search.php
+++ search.php
@@ -22,7 +22,7 @@
                     <form action="search.php" method="GET">
                         <div class="searchBarContainer">
                             <input class="searchBox" type="text">
-                            <button>
+                            <button class="searchButton">
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 172 172" style=" fill:#000000;">
                                     <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                                         <path d="M0,172v-172h172v172z" fill="none"></path>
