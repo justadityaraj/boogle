ca752238f579894f0cb46bbde8207f5040ac44c7
Aditya Raj <justadityaraj@yahoo.com>
2019-08-27 23:36:23 +0530

    img width

--- search.php
+++ search.php
@@ -33,14 +33,7 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
                         <div class="searchBarContainer">
                             <input class="searchBox" type="text">
                             <button class="searchButton">
-                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 172 172" style=" fill:#000000;">
-                                    <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
-                                        <path d="M0,172v-172h172v172z" fill="none"></path>
-                                        <g fill="#397af2">
-                                            <path d="M75.68,13.76c-32.24656,0 -58.48,26.23344 -58.48,58.48c0,32.24656 26.23344,58.48 58.48,58.48c12.76699,0 24.5513,-4.16361 34.17828,-11.13297l45.23735,45.23734l9.72875,-9.72875l-44.82078,-44.82078c8.79817,-10.23729 14.1564,-23.50894 14.1564,-38.03485c0,-32.24656 -26.23344,-58.48 -58.48,-58.48zM48.16,65.36c3.8012,0 6.88,3.0788 6.88,6.88c0,3.8012 -3.0788,6.88 -6.88,6.88c-3.8012,0 -6.88,-3.0788 -6.88,-6.88c0,-3.8012 3.0788,-6.88 6.88,-6.88zM75.68,65.36c3.8012,0 6.88,3.0788 6.88,6.88c0,3.8012 -3.0788,6.88 -6.88,6.88c-3.8012,0 -6.88,-3.0788 -6.88,-6.88c0,-3.8012 3.0788,-6.88 6.88,-6.88zM103.2,65.36c3.8012,0 6.88,3.0788 6.88,6.88c0,3.8012 -3.0788,6.88 -6.88,6.88c-3.8012,0 -6.88,-3.0788 -6.88,-6.88c0,-3.8012 3.0788,-6.88 6.88,-6.88z"></path>
-                                        </g>
-                                    </g>
-                                </svg>
+                                <img src="assets/images/search-icon.png" alt="Search icon">
                             </button>
                         </div>
                     </form>
