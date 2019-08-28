43e877239e8147b54bde2c23c15d0ed40b9c9f9f
Aditya Raj <justadityaraj@yahoo.com>
2019-08-28 21:07:34 +0530

    crawler: substr ./

--- crawler.php
+++ crawler.php
@@ -1,7 +1,7 @@
 <?php
 include("classes/documentParser.php")
 
-function createLinks($src, $url) {
+function createLink($src, $url) {
 
     $scheme = parse_url($url)["scheme"]; //http
     $host = parse_url($url)["host"]; //exm.php
@@ -12,6 +12,10 @@ function createLinks($src, $url) {
     else if (substr($src, 0, 2) == "/") {
         $src = $scheme . "://" . $host . $src;
     }
+    else if (substr($src, 0, 2) == "./"){
+        
+    }
+    return $src;
 }
 function followLinks($url) {
         $parser = new documentParser($url);
@@ -25,6 +29,9 @@ function followLinks($url) {
             else if(substr($href, 0, 11) == "javascript:") {
                 continue;
             }
+
+            $href = $createLink($href, $url);
+            echo $href . "<br>";
         }
 }
 $startUrl = "http://www.bbc.com";
