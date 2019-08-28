cd2e5f9f4543663c913f368d2d8d3f9e195b8e7d
Aditya Raj <justadityaraj@yahoo.com>
2019-08-28 19:31:44 +0530

    crawler: SRC

--- crawler.php
+++ crawler.php
@@ -1,13 +1,23 @@
 <?php
 include("classes/documentParser.php")
+
+function createLinks($src, $url) {
+    echo "SRC: $src \n";
+}
 function followLinks($url) {
         $parser = new documentParser($url);
         $linkList = $parser->getLinks();
         foreach ($linkList as $link) {
             $href = $link->getAtrribute("href");
-            echo $href;
+            
+            if(strpos($href, "#") !== false) {
+                continue;
+            }
+            else if(substr($href, 0, 11) == "javascript:") {
+                continue;
+            }
         }
 }
-$startUrl = "http://www.apple.com";
+$startUrl = "http://www.bbc.com";
 followLinks($startUrl);
 ?>
