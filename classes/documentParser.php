553c67221ee781c45050849c719cfd7fee4ee83d
Aditya Raj <justadityaraj@yahoo.com>
2019-08-28 11:03:38 +0530

    classes/documentParser: stream_context

--- classes/documentParser.php
+++ classes/documentParser.php
@@ -3,6 +3,8 @@ class documentParser {
 
     public function __construct($url) {
         $options = array('http'==>array('method'==>"GET", 'header'==>"user-agent: boogleBot/0.1\n"));
-       
+        $context = stream_context_create($options); 
+        $doc = new DOMDocument();
+        $doc=loadHTML(file_get_contents($url, false, $context));
     }
 }
