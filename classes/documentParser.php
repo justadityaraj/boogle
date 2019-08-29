bc80a4de2842fc18c4ae497cec5fcd62b62a7470
Aditya Raj <45910873+justadityaraj@users.noreply.github.com>
2019-08-30 01:03:40 +0530

    Update documentParser.php

--- classes/documentParser.php
+++ classes/documentParser.php
@@ -5,7 +5,7 @@ class documentParser {
 
     public function __construct($url) {
         $options = array(
-            'http'==>array('method'==>"GET", 'header'==>"user-agent: boogleBot/0.1\n")
+            'http'=>array('method'=>"GET", 'header'=>"user-agent: boogleBot/0.1\n")
         );
         $context = stream_context_create($options); 
         $doc = new DomDocument();
