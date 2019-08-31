2f6caef7073d81cb964aa5701718a45dd4657c85
Aditya Raj <justadityaraj@yahoo.com>
2019-08-31 14:09:17 +0530

    end tag : a in Entity, line: 58

--- search.php
+++ search.php
@@ -1,22 +1,20 @@
 <?php
+
 if (isset($_GET["term"])) {
     $term = $_GET["term"];
 } else {
     exit("You must enter a search term");
 }
-
 $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Boogle</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Welcome to Doodle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
@@ -25,35 +23,34 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
             <div class="headerContent">
                 <div class="logoContainer">
                     <a href="index.php">
-                        <img src="assets/images/boogle-logo.png" alt="">
+                        <img src="assets/images/boogle-logo.png">
                     </a>
                 </div>
                 <div class="searchContainer">
                     <form action="search.php" method="GET">
                         <div class="searchBarContainer">
-                            <input class="searchBox" type="text">
+                            <input class="searchBox" type="text" name="term">
                             <button class="searchButton">
-                                <img src="assets/images/search-icon.png" alt="Search icon">
+                                <img src="assets/images/icons/search.png">
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
-
             <div class="tabsContainer">
                 <ul class="tabList">
                     <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
-                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a>
+                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                            Sites
+                        </a>
                     </li>
                     <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
-                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/justadityaraj/boogle">Repo</a>
+                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                            Images
+                        </a>
                     </li>
                 </ul>
             </div>
-
         </div>
     </div>
 </body>
