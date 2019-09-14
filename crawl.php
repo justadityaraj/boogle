9f39156d7a04d561ca165c92164e669c3499fe1f
Aditya Raj <justadityaraj@yahoo.com>
2019-09-14 14:01:54 +0530

    Descrption & metaTags

--- crawl.php
+++ crawl.php
@@ -4,45 +4,72 @@ include("classes/DomDocumentParser.php");
 $alreadyCrawled = array();
 $crawling = array();
 
-function createLink($src, $url)
-{
+function createLink($src, $url) {
 
 	$scheme = parse_url($url)["scheme"]; // http
 	$host = parse_url($url)["host"]; // www.reecekenney.com
-
-	if (substr($src, 0, 2) == "//") {
+	
+	if(substr($src, 0, 2) == "//") {
 		$src =  $scheme . ":" . $src;
-	} else if (substr($src, 0, 1) == "/") {
+	}
+	else if(substr($src, 0, 1) == "/") {
 		$src = $scheme . "://" . $host . $src;
-	} else if (substr($src, 0, 2) == "./") {
+	}
+	else if(substr($src, 0, 2) == "./") {
 		$src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
-	} else if (substr($src, 0, 3) == "../") {
+	}
+	else if(substr($src, 0, 3) == "../") {
 		$src = $scheme . "://" . $host . "/" . $src;
-	} else if (substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+	}
+	else if(substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
 		$src = $scheme . "://" . $host . "/" . $src;
 	}
 
 	return $src;
 }
-function getDetails($url)
-{
-	$parser = new DomDocumentParser($url);
-	$titleArray = $parser->getTitletags();
 
-	if (sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
+function getDetails($url) {
+
+	$parser = new DomDocumentParser($url);
+
+	$titleArray = $parser->getTitleTags();
+
+	if(sizeof($titleArray) == 0 || $titleArray->item(0) == NULL) {
 		return;
 	}
 
 	$title = $titleArray->item(0)->nodeValue;
 	$title = str_replace("\n", "", $title);
-	if ($title == "") {
+
+	if($title == "") {
 		return;
 	}
-	echo "URL: $url, Title: $title<br>";
+
+	$description = "";
+	$keywords = "";
+
+	$metasArray = $parser->getMetatags();
+
+	foreach($metasArray as $meta) {
+
+		if($meta->getAttribute("name") == "description") {
+			$description = $meta->getAttribute("content");
+		}
+
+		if($meta->getAttribute("name") == "keywords") {
+			$keywords = $meta->getAttribute("content");
+		}
+	}
+
+	$description = str_replace("\n", "", $description);
+	$keywords = str_replace("\n", "", $keywords);
+
+
+	
+
 }
 
-function followLinks($url)
-{
+function followLinks($url) {
 
 	global $alreadyCrawled;
 	global $crawling;
@@ -51,12 +78,13 @@ function followLinks($url)
 
 	$linkList = $parser->getLinks();
 
-	foreach ($linkList as $link) {
+	foreach($linkList as $link) {
 		$href = $link->getAttribute("href");
 
-		if (strpos($href, "#") !== false) {
+		if(strpos($href, "#") !== false) {
 			continue;
-		} else if (substr($href, 0, 11) == "javascript:") {
+		}
+		else if(substr($href, 0, 11) == "javascript:") {
 			continue;
 		}
 
@@ -64,19 +92,23 @@ function followLinks($url)
 		$href = createLink($href, $url);
 
 
-		if (!in_array($href, $alreadyCrawled)) {
+		if(!in_array($href, $alreadyCrawled)) {
 			$alreadyCrawled[] = $href;
 			$crawling[] = $href;
 
 			getDetails($href);
-		} else return;
+		}
+		else return;
+
+
 	}
 
 	array_shift($crawling);
 
-	foreach ($crawling as $site) {
+	foreach($crawling as $site) {
 		followLinks($site);
 	}
+
 }
 
 $startUrl = "http://www.bbc.com";
--- index.php
+++ index.php
@@ -2,32 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Boogle</title>
+    <meta name="description" content="minimal web search for sites and images">
+    <meta name="keywords" content="boogle search engine, google responsive clone,google by aditya raj">
+    <meta name="author" content="Aditya Raj">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- Matomo -->
-    <script type="text/javascript">
-        var _paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u = "//localhost/matomo/";
-            _paq.push(['setTrackerUrl', u + 'matomo.php']);
-            _paq.push(['setSiteId', '2']);
-            var d = document,
-                g = d.createElement('script'),
-                s = d.getElementsByTagName('script')[0];
-            g.type = 'text/javascript';
-            g.async = true;
-            g.defer = true;
-            g.src = u + 'matomo.js';
-            s.parentNode.insertBefore(g, s);
-        })();
-    </script>
-    <!-- End Matomo Code -->
 </head>
 
 <body>
